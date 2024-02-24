4675fabb5ca18c898f8318858cee70dba30bd730
Tithi Mehta <dharamehta012@gmail.com>
2024-02-24 17:48:02 +0530

    review router

--- synergy_binarybrigades/src/App.tsx
+++ synergy_binarybrigades/src/App.tsx
@@ -1,26 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import DemoSidebar from './components/SideBar1/sidebar'
-import Navigation from './components/Navigation'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import DemoSidebar from './components/SideBar1/sidebar';
+import Navigation from './components/Navigation';
+import { ThreeDCardDemo } from './components/Card';
+import Review from './Review';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <div className="headersection flex-1 max-w-10">
-    <div>
-      <Navigation/>
+    <Router>
+      <div className="headersection flex">
+        {/* <DemoSidebar /> */}
+        <Routes>
+          <Route path="/reviews" element={<Review />} />
+        </Routes>
       </div>
-      <div>
-      <DemoSidebar/>
-      </div>
-      
-    </div>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
+  {/* <div className='flex flex-col'>
+    <div className='flex ml-4vw justify-center items-center gap-10'>
+      <ThreeDCardDemo />
+      <ThreeDCardDemo />
+      <ThreeDCardDemo />
+    </div>
+    <div className='flex ml-4vw justify-center items-center gap-10'>
+      <ThreeDCardDemo />
+      <ThreeDCardDemo />
+      <ThreeDCardDemo />
+    </div>
+  </div> */}
--- synergy_binarybrigades/src/components/SideBar1/sidebar.tsx
+++ synergy_binarybrigades/src/components/SideBar1/sidebar.tsx
@@ -1,11 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-
-import React from 'react'
 
 export default function DemoSidebar() {
 	return (
 	  <>
-		<div className="h-full p-0 ml-1vw space-y-2 w-60 bg-gray-900 text-gray-100 fixed left-0">
+		<div className="h-full p-0 space-y-2 w-60 bg-gray-900 text-gray-100 px-0 left-0">
 		  <div className="flex items-center pt-2 m-3 space-x-2">
 			<img
 			  src="https://source.unsplash.com/100x100/?portrait"
@@ -63,13 +63,13 @@ export default function DemoSidebar() {
 				</a>
 			</li>
 			<li>
-				<a rel="noopener noreferrer" href="#" className="p-3 hover:bg-gray-700 rounded-xl flex items-center p-2 space-x-3 rounded-md">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current dark:text-gray-400">
-						<path d="M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z"></path>
-					</svg>
-					<span>Wishlist</span>
-				</a>
-			</li>
+          <Link to="/reviews" className="p-3 hover:bg-gray-700 rounded-xl flex items-center p-2 space-x-3 rounded-md">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current dark:text-gray-400">
+              <path d="M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z"></path>
+            </svg>
+            <span>Reviews</span>
+          </Link>
+        </li>
 		</ul>
 		<ul className="pt-4 pb-2 m-4 space-y-1 text-sm">
 			<li>
