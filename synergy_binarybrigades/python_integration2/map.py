9188566c939041fa3baa04450ce6645b057ec970
vaishnavi shridhar <vaishnavishridhar23@gmail.com>
2024-02-24 23:16:51 +0530

    Integration fixed

---
+++ synergy_binarybrigades/python_integration2/map.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, jsonify, request
+import openrouteservice as ors
+from flask_cors import CORS, cross_origin
+ 
+ 
+app = Flask(__name__)
+ 
+# Replace 'YOUR_API_KEY' with your actual API key
+CLIENT = ors.Client(key='5b3ce3597851110001cf6248266c3dd67e9b43748ff1423ec5f335c6')
+cors = CORS(app)
+ 
+@app.route('/')
+def serve_react_app():
+    return render_template('index.html')
+ 
+@app.route('/fetch_map', methods=['POST'])
+def fetch_map():
+    data = request.json
+    start = (float(data['startLon']), float(data['startLat']))
+    end = float(data['endLon']), float(data['endLat'])
+ 
+    # Directions (shortest route)
+    directions = CLIENT.directions(
+        coordinates=[start, end],
+        profile='driving-car',
+        format='geojson'
+    )
+ 
+    # Route coordinates
+    coordinates = []
+    for feature in directions['features']:
+        for coord in feature['geometry']['coordinates']:
+            coordinates.append(coord)
+ 
+    # EVs on the way (1km radius)
+    pois = CLIENT.places(
+        request='pois',
+        geojson={'type': 'LineString', 'coordinates': coordinates},
+        buffer=1000,
+        filter_category_ids=[596]
+    )
+ 
+   
+    ev_coordinates = [list(reversed(poi['geometry']['coordinates'])) for poi in pois['features']]
+ 
+    return jsonify({'coordinates': coordinates, 'ev_coordinates': ev_coordinates})
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
