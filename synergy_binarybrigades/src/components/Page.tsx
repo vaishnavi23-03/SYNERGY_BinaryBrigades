527b5c237af83d84ea3ba30d46ed7e59722e88c0
Ashish <ashishsatavase13@gmail.com>
2024-02-24 16:36:38 +0530

    ab

---
+++ synergy_binarybrigades/src/components/Page.tsx
@@ -0,0 +1,10 @@
+const Page= ()=>{
+    return(
+        <>
+            <div className="bg-red border-2 border-gray-200 h-full font-serif flex justify-center">
+                <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" alt="" />
+                </div>
+        </>
+    );
+}
+export default Page;
